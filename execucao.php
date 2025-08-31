3dfb87526d37f0d8b1628640ecce3c09bedcadf3
Isa Araújo <isabelaa.ifpr2024@gmail.com>
2025-08-31 20:07:59 -0300

    Update execucao.php

--- execucao.php
+++ execucao.php
@@ -3,113 +3,110 @@
 require_once("modelo/Pedido.php");
 require_once("modelo/Prato.php");
 
-//Função para listar os pratos
-function listarPratos($pratos)
-{
-    if (count($pratos) > 0) {
-        foreach ($pratos as $i => $prato)
-            printf(
-                "%d- Número: %s | Nome: %s | Valor: %f\n",
-                $i + 1,
-                $prato->getNumero(),
-                $prato->getNome(),
-                $prato->getValor()
-            );
-    } else
+//Função para listar os pedidos
+function listarPedidos ($pedidos) {
+    if(count($pedidos) > 0) {
+        foreach($pedidos as $i => $pedido)
+            printf("O cliente " . 
+    } else 
         echo "Nenhum prato cadastrado.\n";
 }
 
-
-function retornaPratos($pratos, $numero)
-{
-
-    foreach ($pratos as $p) {
-        if ($numero == $p->getNumero())
+function retornarPrato($pratos, $numero) {
+    foreach($pratos as $p) {
+        if($numero == $p->getNumero())
             return $p;
     }
 
     return null;
 }
 
-$prato1 = new Prato;
-$prato1->setNumero(1);
-$prato1->setNome("Camarão à Milanesa");
-$prato1->setValor(110);
 
-$prato2 = new Prato;
-$prato2->setNumero(2);
-$prato2->setNome("Pizza Margherita");
-$prato2->setValor(80);
+$p1 = new Prato;
+$p1->setNumero(1);
+$p1->setNome("Camarão à milanesa");
+$p1->setValor(110);
 
-$prato3 = new Prato;
-$prato3->setNumero(3);
-$prato3->setNome("Macarrão à Carbonara");
-$prato3->setValor(60);
+$p2 = new Prato;
+$p2->setNumero(2);
+$p2->setNome("Pizza Margherita");
+$p2->setValor(80);
 
-$prato4 = new Prato;
-$prato4->setNumero(4);
-$prato4->setNome("Bife à Parmegiana");
-$prato4->setValor(75);
+$p3 = new Prato;
+$p3->setNumero(3);
+$p3->setNome("Macarrão à Carbonara");
+$p3->setValor(60);
 
-$prato5 = new Prato;
-$prato5->setNumero(5);
-$prato5->setNome("Risoto ao Fungi");
-$prato5->setValor(70);
+$p4 = new Prato;
+$p4->setNumero(4);
+$p4->setNome("Bife à Parmegiana");
+$p4->setValor(75);
+
+$p5 = new Prato;
+$p5->setNumero(5);
+$p5->setNome("Risoto ao Funghi");
+$p5->setValor(70);
 
 $pratos = array(
-    $prato1,
-    $prato2,
-    $prato3,
-    $prato4,
-    $prato5
+    $p1, $p2, $p3, $p4, $p5
 );
 
 //menu
-
 do {
-    echo "\n\n------MENU------\n";
-    echo "1- Cadastrar pedido\n";
-    echo "2- Cancelar pedido\n";
-    echo "3- Listar pedidos\n";
-    echo "4- Total de vendas: ";
+    echo "----MENU----\n";
+    echo "1- Cadastrar\n";
+    echo "2- Cancelar \n";
+    echo "3- Listar \n";
+    echo "4- Total de vendas \n";
     echo "0- Sair\n";
-    $opcao = readline("Informe a opção: ");
+    $opcao = readline ("Informe a opção: ");
 
     echo "\n";
 
-    switch ($opcao) {
+    switch($opcao){
         case 1:
-            $pedido = new Pedido;
+            $pedido = new Pedido();
             $pedido->setNomeCliente(readline("Informe o nome do cliente: "));
             $pedido->setNomeGarcom(readline("Informe o nome do garçom: "));
-
-            foreach ($pratos as $p) {
+            
+            //Exibir os pratos para usuário
+            foreach($pratos as $p) {
                 echo $p->getNumero() . " - " . $p->getNome() . " - " . $p->getValor() . "\n";
             }
 
+            //Informar o número do prato que ele quer
+            $numeroPrato = readline("Informe o número do prato: ");
+            $prato = retornarPrato($pratos, $numeroPrato);
+            $pedido->setPrato($prato);
 
-
-            break;
-
-        case 2;
-            //cancelar
-            break;
-
-        case 3;
-            foreach ($pedidos as $pedido) {
-                print("O cliente " . $pedido->setNomeCliente() . ", foi atendido pelo garçom " . $pedido->setNomeGarcom() . ", pediu um prato de " . $pedido->setPrato() . ", no valor de R$" . $pedido->setValor());
+        case 2:
+            //Cancelar
+             listarPedidos($pedidos);
+            if(count($pedidos) > 0) {
+                $idx = readline("Informe o número do pedido para excluir: ");
+                if($idx > 0 && $idx <= count($pedidos))
+                    array_splice($pedidos, $idx-1, 1);
+                else
+                    echo "Número inválido!\n";
             }
             break;
 
-        case 4;
-            //total
+        case 3:
+            //Listar
+            foreach ($pedidos as $pedido) {
+                print("O cliente " . $pedido->getNomeCliente() . ", foi atendido pelo garçom " . $pedido->setNomeGarcom() . ", pediu um prato de " . $pedido->setPrato() . ", no valor de R$" . $pedido->setValor());
+            }
+            break;
+
+        case 4:
+
             break;
 
         case 0:
-            echo "Programa encerrado!\n";
+            echo "Programa encerrado! \n";
             break;
-
+        
         default:
             echo "Opção inválida!\n";
     }
-} while ($opcao != 0);
+}while($opcao != 0);
